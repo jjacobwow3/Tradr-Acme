1edbc126c48c08ceda9daaf71501c2c8762c0ed7
Bleedem <jjacobwow@gmail.com>
2024-08-19 04:18:58 -0400

    font bug fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,4 @@ import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-const lusitana = Lusitana({
-    subsets: ['latin'], // You can specify the subsets you need
-    weight: ['400', '700'], // Optional: specify the font weights you want to use
-  });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'], });
